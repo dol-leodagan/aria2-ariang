ea3fda42f4f3a6f91db0649364030e6850102f60
dol-leodagan <leodagan-github@durron.net>
2018-07-11 07:47:16 +0200

    Moving from OpenSSL to LibreSSL. Removing bash dependencies  (#3)
    * Moving from OpenSSL to LibreSSL. Removing bash dependencies

--- files/on_download_complete.sh
+++ files/on_download_complete.sh
@@ -1,21 +1,23 @@
-#!/usr/bin/env bash
+#!/bin/sh
 
 echo "Moving Completed Download Files..."
 if [ "$2" -ne 0 ]; then
 
     # Skip torrent file
-    if [[ "$3" == *.torrent ]]; then
-        echo "Not moving .torrent File..."
-        exit
-    fi
+    case "$3" in
+        *.torrent)
+            echo "Not moving .torrent File..."
+            exit
+            ;;
+    esac
 
     # Find Download TopLevel Dir
     TOP_LEVEL_DIR="$3"
     while [ "$(dirname "$TOP_LEVEL_DIR")" != "$ARIA2_DOWNLOAD_DIR" ]; do
-        if [ "$TOP_LEVEL_DIR" == "/" ] || [ "$TOP_LEVEL_DIR" == "." ]; then
-        echo "File $3 is not in $ARIA2_DOWNLOAD_DIR download directory."
-        exit
-    fi
+        if [ "$TOP_LEVEL_DIR" = "/" ] || [ "$TOP_LEVEL_DIR" = "." ]; then
+            echo "File $3 is not in $ARIA2_DOWNLOAD_DIR download directory."
+            exit
+        fi
         TOP_LEVEL_DIR="$(dirname "$TOP_LEVEL_DIR")"
     done
 
